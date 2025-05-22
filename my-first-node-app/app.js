4754159d6d48a6105a28f7f95f84a91f818af64e
DESKTOP-T4JO1P7\ryf <286144476@qq.com>
2025-05-22 20:07:00 +0900

    切换到新分支的内容

--- my-first-node-app/app.js
+++ my-first-node-app/app.js
@@ -4,13 +4,13 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Welcome to the homepage!');
+        res.end('Welcome to the homepage!新分支上的');
       } else if (req.url === '/about') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('This is the about page.');
+        res.end('This is the about page新分支上的.');
       } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
+        res.end('404 Not Found新分支上的');
       }
 });
 
